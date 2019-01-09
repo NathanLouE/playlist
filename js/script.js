b139e20a0ec32426bcbaebe14d03e07afb5792d2
Nathan Louie <nathanlou22@gmail.com>
2019-01-09 02:07:28 +0000

    foreach

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 /* global $ */
 
 // BELOW Update the songs to your favorites songs. There should be at least 4.
-var songs = [
-    "Havana",
-    "Lifelight",
+var songs = ["Havana",
+    "Last Surprise(Persona 5)",
     "Rockefeller Street(Nightcore Version)",
     "Flamingo",
-    "Rivers in the Desert(Persona 5)"
-];
+    "Rivers in the Desert(Persona 5)"];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
-var images_urls=[];
-var artists=[];
+var images_urls=["http://d1lofqbqbj927c.cloudfront.net/exa/2018/01/23091006/290241501786508-300x300.jpg",
+    "https://i.ytimg.com/vi/aZZvTzdTurA/maxresdefault.jpg",
+    ""];
+var artists=["Camila Cabello", 
+    "Lyn Inaizumi", 
+    "Getter Jaani", 
+    "Kero Kero Bonito", 
+    "Lyn Inaizumi"];
 var lengths=[];
 var links=[];
 
@@ -24,7 +28,23 @@ function displaySongInfo(){
         $("#songs").append("<p>" + song + "</p>"); 
     });
     // BELOW Use forEach Loop to display the data from each of your array's in the correct div
-
+    images_urls.forEach(function(img) {
+        var url = `<img src= ${img}>`;
+        $("#images").append(url);
+    });
+    
+    artists.forEach(function(people) {
+        $("#artist").append(people);
+    });
+    
+    lengths.forEach(function(secs){
+        $("#length").append(secs)
+    });
+    
+    links.forEach(function(links) {
+        var url = `<a href ${links} >`;
+        $("#link").append(url);
+    })
 }
 
 function emptySongInfo(){
